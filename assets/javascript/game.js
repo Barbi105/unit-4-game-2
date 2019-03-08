a086294c25246223b175c3eef634cf089a22fab5
barbi105 <bontiverosg@gmail.com>
2019-03-07 22:14:02 -0600

    initial documents and pseudocode

---
+++ assets/javascript/game.js
@@ -0,0 +1,44 @@
+// Star Wars RPG!
+
+// we have 4 characters: object?
+    // each has: 
+    // different level life
+    // different attack number: number is multiplied by the the times the attack button is clicked
+    // different counter attack number: number stays constant
+
+// make variables for health points.
+
+// reset function
+// display words pick your character
+// characters are floating in neutral ground
+// reset health points
+
+// click on a character to becomes me: function()
+    //chosen character moves to battlefield 
+    // all other characters become enemies
+        // display words pick your enemy delayed 2 seconds  
+        //change color to red 
+        
+// click on a remaining character for it to become rival: function()
+    //chosen character moves to battlefield
+    //other enemies dim out. opacity decreases?
+    //display word "good luck"
+    //display word "_"
+
+// clicking the attack button prompts
+    // function: attack()
+    // me attack decreases rival health points.
+        // number is multiplied by times attack botton is clicked
+    // enemy counter attack decreases me health points
+
+    // if enemy 1 health points <= 0: 
+        // picture dissapears 
+        // text round 2 appears
+        // characters come back to light
+        // pick new enemy character
+    // else if me health points <= 0:
+        // enemy wins
+        // game is restart
+  
+
+
